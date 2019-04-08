d1370135b2a3e4db2e6f0b2fd6c8dfdfffa8b5db
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-08 17:15:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,9 @@ def reverse_each_word(string_sentence)
   original_arr = string_sentence.split(" ")
   new_arr = []
   original_arr.each do |r|
-     new_arr << r.reverse
+  new_arr << r.reverse
 end
   new_arr.join(" ")
   
 end
+
