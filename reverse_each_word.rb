401470dd6e433b8b04a5b90220cd3c6459ec83ff
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-08 17:10:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,5 +4,9 @@ def reverse_each_word(string_sentence)
 end
 end
 
-array = ["Hello there, and how are you?"]
-reverse_each_word(array)
+original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
